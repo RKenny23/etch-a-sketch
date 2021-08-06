25b8bce6e25ef83fd20a236673262612e2018bbc
RKenny23 <manifestatio@live.com>
2021-08-06 10:40:35 -0400

    got mouseover working.

--- script.js
+++ script.js
@@ -1,66 +1,62 @@
-// Select the container made in html file
+// Select the containers pre-made in html file
+const btnContainer = document.querySelector('.buttons');
 const gridContainer = document.querySelector('.grid-container'); 
 
-const btnContainer = document.querySelector('buttons');
-
 // Create the buttons
-
-const blackBtn = document.createElement('button');
-const sizeBtn = document.createElement('button');
 const resetBtn = document.createElement('button');
+const blackBtn = document.createElement('button');
 
+// Add the buttons to the button container
+btnContainer.appendChild(resetBtn).classList.add('btn');
+btnContainer.appendChild(blackBtn).classList.add('btn');
 
-
-document.body.appendChild(resetBtn);
-resetBtn.style.width = '80px';
-resetBtn.style.height = '80px';
-resetBtn.style.backgroundColor = 'red';
-
-document.body.appendChild(blackBtn);
-blackBtn.style.width = '80px';
-blackBtn.style.height = '80px';
+// Style the buttons
+resetBtn.innerHTML = 'reset';
 blackBtn.style.backgroundColor = 'black';
 
-
-
-  // if (userInput != null) {
-  //   let gridSize = userInput;
-  // })
-  // resetBtn.addEventListener('click', () => {
-  //   prompt("Enter a grid size", 16);  
-    
-   
-
-
-// Create divs to go in the container. # based on user input
+// Create divs to go in the container
 function createGrid(cols, rows) {
-  for (let i = 0; i < cols; i++) {
-    const newDiv = document.createElement('div');
-    
-    // newDiv.style.border = '1px solid black';
+  for (i = 0; i < (cols * rows); i++) {
+    const newDiv = document.createElement('div');    
     gridContainer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`;    
-    gridContainer.appendChild(newDiv).classList.add('box');
+    gridContainer.appendChild(newDiv).className = 'box';
   }
 }
 
-
-// Call the function to make the grid based on user input
-
 createGrid(16, 16);
 
-//Select all the divs with the 'box' class
-// function colorBlack() {
-//   const boxes = gridContainer.querySelectorAll('.box');
-//   btnBlack.style.background = 'black';
-//   btnBlack.textAlign = 'center';
-//   bbtnBlack.addEventListener('click', () => {
-//     boxes.forEach(box => box.addEventListener('mouseover', () => {
-//       box.style.background = 'black';
-//     }))
-    
-//   })
+// Add mouseover event for the new divs
+function drawBlack() {
+  const boxes = gridContainer.querySelectorAll('.box')
+  boxes.forEach(box => {
+    box.addEventListener('mouseover', e => {
+      e.target.style.backgroundColor = '#000';
+    })
+  })
+}
 
-//   gridContainer.appendChild(btnGray).classList.add('btn')
+drawBlack()
 
-// 
+// Lets user draw black when black button is pressed
+blackBtn.addEventListener('click', () => {
+  drawBlack();
+})
+
+// Removes the boxes from the grid
+function clearGrid() {
+  const boxes = gridContainer.querySelectorAll('.box');
+  boxes.forEach(box => {
+    box.remove();
+  })
+}
+
+// Allows user to input custom grid size
+resetBtn.addEventListener('click', () => {
+  let userInput = prompt("Enter a grid size:", 16);  
+  if (userInput != null) {
+    clearGrid();
+    createGrid(userInput, userInput);
+  }
+})
+
